079c738bb92c56bd47b3862328775c850065ddf1
René Dohmen <acidjunk@gmail.com>
2021-06-18 20:29:52 +0200

    Sync

--- server/settings.py
+++ server/settings.py
@@ -13,12 +13,15 @@
 
 import secrets
 import string
-from typing import List
+from typing import List, Optional, Dict, Any
 
 from pydantic import BaseSettings
+from pydantic import validator
+from pydantic.networks import EmailStr, PostgresDsn
 
 
 class AppSettings(BaseSettings):
+    PROJECT_NAME: str = "Boilerplate webservice"
     TESTING: bool = True
     SESSION_SECRET: str = "".join(secrets.choice(string.ascii_letters) for i in range(16))  # noqa: S311
     OAUTH_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
@@ -42,16 +45,55 @@ class AppSettings(BaseSettings):
     SWAGGER_HOST: str = "localhost"
     GUI_URI: str = "http://localhost:3000"
     DATABASE_URI: str = "postgresql://boilerplate:boilerplate@localhost/boilerplate"
+
+    @validator("DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host=values.get("POSTGRES_SERVER"),
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
+
     MAX_WORKERS: int = 5
-    MAIL_SERVER: str = "localhost"
-    MAIL_PORT: int = 25
-    MAIL_STARTTLS: bool = False
     CACHE_HOST: str = "127.0.0.1"
     CACHE_PORT: int = 6379
     POST_MORTEM_DEBUGGER: str = ""
     SERVICE_NAME: str = "Boilerplate"
     LOGGING_HOST: str = "localhost"
     LOG_LEVEL: str = "DEBUG"
+
+    # Mail settings
+    SMTP_TLS: bool = True
+    SMTP_PORT: Optional[int] = None
+    SMTP_HOST: Optional[str] = None
+    SMTP_USER: Optional[str] = None
+    SMTP_PASSWORD: Optional[str] = None
+    EMAILS_FROM_EMAIL: Optional[EmailStr] = None
+    EMAILS_FROM_NAME: Optional[str] = None
+
+    @validator("EMAILS_FROM_NAME")
+    def get_project_name(cls, v: Optional[str], values: Dict[str, Any]) -> str:
+        if not v:
+            return values["PROJECT_NAME"]
+        return v
+
+    EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
+    EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/build"
+    EMAILS_ENABLED: bool = False
+
+    @validator("EMAILS_ENABLED", pre=True)
+    def get_emails_enabled(cls, v: bool, values: Dict[str, Any]) -> bool:
+        return bool(
+            values.get("SMTP_HOST")
+            and values.get("SMTP_PORT")
+            and values.get("EMAILS_FROM_EMAIL")
+        )
+
+    EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
 
     class Config:
         env_file = ".env"
