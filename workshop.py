9d8083f66377a2c34ae1c440fb9006f06e5b9cb8
René Dohmen <acidjunk@gmail.com>
2021-10-23 04:26:45 +0200

    Initial commit of workshop pydantic stuff

---
+++ workshop.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr
+
+UUIDstr = str
+
+class User(BaseModel):
+    id: UUIDstr
+    name = 'John Doe'
+    age: Optional[int] = None
+    email: EmailStr
+    signup_ts: Optional[datetime] = None
+    friends: List[int] = []
+
+
+external_data = {
+    'id': '123',
+    'signup_ts': '2019-06-01 12:22',
+    'friends': [1, 2, '3'],
+}
+user = User(**external_data)
+print(user.id)
+#> 123
+print(repr(user.signup_ts))
+#> datetime.datetime(2019, 6, 1, 12, 22)
+print(user.friends)
+#> [1, 2, 3]
+print(user.dict())
+"""
+{
+    'id': 123,
+    'signup_ts': datetime.datetime(2019, 6, 1, 12, 22),
+    'friends': [1, 2, 3],
+    'name': 'John Doe',
+}
+"""
